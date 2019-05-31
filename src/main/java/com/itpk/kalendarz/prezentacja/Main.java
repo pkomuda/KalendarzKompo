f9393d8c34b975e394e7bed9fec604cc84924805
pkomuda <p.komuda1@gmail.com>
2019-05-31 11:36:26 +0200

    if zamiast switcha z argumentami programu

--- src/main/java/com/itpk/kalendarz/prezentacja/Main.java
+++ src/main/java/com/itpk/kalendarz/prezentacja/Main.java
@@ -4,130 +4,114 @@ import com.itpk.kalendarz.logika.RepozytoriumDni;
 import com.itpk.kalendarz.prezentacja.gui.Kalendarz;
 
 import java.awt.*;
-import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Main
 {
+    public static void main(String[] args)
+    {
+        if (args.length==1 && (args[0].equals("c") || args[0].equals("C")))
+        {
+            int wybor = 1;
+            String opis;
+            String miejsce;
+            Calendar data;
+            int rok;
+            int miesiac;
+            int dzien;
+            int indeks;
+            String s;
 
-    public static void main(String[] args) {
-        switch (args[0]) {
-            case "c":
-            case "C":
+            RepozytoriumDni dni = new RepozytoriumDni();
+            while (wybor != 0)
+            {
+                System.out.println("Menu");
+                System.out.println("1.Dodaj wydarzenie");
+                System.out.println("2.Usuń wydarzenie");
+                System.out.println("3.Wyswietl wszystkie wydarzenia");
+                System.out.println("4.Usuń wydarzenia starszze niż (dzien, miesiac, rok)");
+                System.out.println("5.Filtruj wydarzenia");
+                System.out.println("6.O programie");
+                System.out.println("0.Wyjdź z programu");
+                Scanner scanner = new Scanner(System.in);
+                wybor = scanner.nextInt();
 
-                int wybor = 1;
-                String opis;
-                String miejsce;
-                Calendar data;
-                int rok;
-                int miesiac;
-                int dzien;
-                int indeks;
-                String s;
-
-                RepozytoriumDni dni = new RepozytoriumDni();
-                while (wybor != 0)
+                switch (wybor)
                 {
-                    System.out.println("Menu");
-                    System.out.println("1.Dodaj wydarzenie");
-                    System.out.println("2.Usuń wydarzenie");
-                    System.out.println("3.Wyswietl wszystkie wydarzenia");
-                    System.out.println("4.Usuń wydarzenia starszze niż (dzien, miesiac, rok)");
-                    System.out.println("5.Filtruj wydarzenia");
-                    System.out.println("6.O programie");
-                    System.out.println("0.Wyjdź z programu");
-                    Scanner scanner = new Scanner(System.in);
-                    wybor = scanner.nextInt();
-
-                    switch (wybor)
-                    {
-                        case 1:
-                            System.out.print("opis: ");
-                            opis=scanner.next();
-                            System.out.print("miejsce: ");
-                            miejsce=scanner.next();
-                            do
-                            {
-                                System.out.print("data(YYYY MM DD): ");
-                                rok = scanner.nextInt();
-                                miesiac = scanner.nextInt();
-                                dzien = scanner.nextInt();
-                            }
-                            while (miesiac<1 || miesiac>12 || dzien<1 || dzien>31);
-                            dni.dodajWydarzenie(opis,miejsce,new GregorianCalendar(rok,miesiac,dzien));
-                            break;
-
-                        case 2:
+                    case 1:
+                        System.out.print("opis: ");
+                        opis = scanner.next();
+                        System.out.print("miejsce: ");
+                        miejsce = scanner.next();
+                        do
+                        {
                             System.out.print("data(YYYY MM DD): ");
-                            rok=scanner.nextInt();
-                            miesiac=scanner.nextInt();
-                            dzien=scanner.nextInt();
-                            System.out.print("Nr wydarzenia: ");
-                            indeks=scanner.nextInt();
-                            dni.usunWydarzenie(new GregorianCalendar(rok,miesiac,dzien),indeks);
-                            break;
+                            rok = scanner.nextInt();
+                            miesiac = scanner.nextInt();
+                            dzien = scanner.nextInt();
+                        }
+                        while (miesiac < 1 || miesiac > 12 || dzien < 1 || dzien > 31);
+                        dni.dodajWydarzenie(opis, miejsce, new GregorianCalendar(rok, miesiac, dzien));
+                        break;
 
-                        case 3:
-                            System.out.println(dni.toString());
-                            break;
+                    case 2:
+                        System.out.print("data(YYYY MM DD): ");
+                        rok = scanner.nextInt();
+                        miesiac = scanner.nextInt();
+                        dzien = scanner.nextInt();
+                        System.out.print("Nr wydarzenia: ");
+                        indeks = scanner.nextInt();
+                        dni.usunWydarzenie(new GregorianCalendar(rok, miesiac, dzien), indeks);
+                        break;
 
-                        case 4:
-                            System.out.print("data(YYYY MM DD): ");
-                            rok=scanner.nextInt();
-                            miesiac=scanner.nextInt();
-                            dzien=scanner.nextInt();
-                            dni.usunWydarzeniaStarszeNiz(dzien,miesiac,rok);
-                            break;
+                    case 3:
+                        System.out.println(dni.toString());
+                        break;
 
-                        case 5:
-                            System.out.println("Szukaj: ");
-                            s=scanner.next();
-                            dni.wydarzeniaZawierajace(s);
-                            break;
+                    case 4:
+                        System.out.print("data(YYYY MM DD): ");
+                        rok = scanner.nextInt();
+                        miesiac = scanner.nextInt();
+                        dzien = scanner.nextInt();
+                        dni.usunWydarzeniaStarszeNiz(dzien, miesiac, rok);
+                        break;
 
-                        case 6:
-                            System.out.println(RepozytoriumDni.oProgramie());
-                            break;
+                    case 5:
+                        System.out.println("Szukaj: ");
+                        s = scanner.next();
+                        dni.wydarzeniaZawierajace(s);
+                        break;
 
-                        case 0:
-                            break;
+                    case 6:
+                        System.out.println(RepozytoriumDni.oProgramie());
+                        break;
 
-                        default:
-                            System.out.println("Nie ma takiej opcji");
+                    case 0:
+                        break;
 
-                    }
+                    default:
+                        System.out.println("Nie ma takiej opcji");
+
                 }
-
-
-
-
-
-
-
-
-
-                break;
-
-
-            case "":
-                EventQueue.invokeLater(() -> {
-                    try
-                    {
-                        Kalendarz frame = new Kalendarz();
-                        frame.setLocationRelativeTo(frame);
-                        frame.setVisible(true);
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                    }
-                });
-
-            default:
-                break;
+            }
+        }
+        else
+        {
+            EventQueue.invokeLater(() ->
+            {
+                try
+                {
+                    Kalendarz frame = new Kalendarz();
+                    frame.setLocationRelativeTo(frame);
+                    frame.setVisible(true);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            });
         }
     }
 }
